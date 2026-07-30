4123af292d6af255b1161df09bf6a40899436d71
Emiliano Chontal <nanochontal@gmail.com>
2026-07-29 20:02:39 -0600

    New commit

--- app/register.tsx
+++ app/register.tsx
@@ -11,7 +11,7 @@ export default function LoginScreen() {
 
     useEffect(() => {
         iniciarSesion();
-    }, []);
+    }, [correo, password]);
 
     const iniciarSesion = async () => {
         try {
